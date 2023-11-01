5110778de88da00e9d237cf23ac76ce32d9083c5
Bilal <aslambilal@gmail.com>
2023-11-01 10:14:02 -0400

    Fixed references to match Shiny Assembly-CSharp

--- Projected Damage/Plugin.cs
+++ Projected Damage/Plugin.cs
@@ -3,22 +3,43 @@ using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
 
+using InkboundModEnabler.Util;
+using ShinyShoe;
+using ShinyShoe.Ares; // Contains EntityHandle
+
 namespace Projected_Damage
 {
-    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
-    public class ProjectedDamagePlugin : BaseUnityPlugin
-    {
-        public const string PLUGIN_GUID = "Projected_Damage";
-        public const string PLUGIN_NAME = "Projected Damage";
-        public const string PLUGIN_VERSION = "0.0.0";
-        public static readonly Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
-        internal static ManualLogSource log;
-        private void Awake()
-        {
-            // Plugin startup logic
-            log = Logger;
-            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
-            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
-        }
-    }
+	[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+	[CosmeticPlugin]
+	public class ProjectedDamagePlugin : BaseUnityPlugin
+	{
+		public const string PLUGIN_GUID = "Projected_Damage";
+		public const string PLUGIN_NAME = "Projected Damage";
+		public const string PLUGIN_VERSION = "0.0.0";
+		public static readonly Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
+		internal static ManualLogSource log;
+		private void Awake()
+		{
+			// Plugin startup logic
+			log = Logger;
+			HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+			Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+		}
+
+		// Code from ADDB 
+		[HarmonyPatch(typeof(WorldUnitControllerContent))]
+		public static class WorldUnit_Patch
+		{
+			public static int GetHPDiffBetweenSimulation(EntityHandle entityHandle)
+			{
+				var app_state = ClientApp.Inst.GetStateRo_Debug();
+				var oldStats = UnitCombatDBHelper.GetHpEnergyShieldStats(entityHandle, app_state.GetWorldClientRo().GetWorldStateRo().GetUnitCombatDBRo());
+				var newStats = UnitCombatDBHelper.GetHpEnergyShieldStats(entityHandle, app_state.GetCombatPreviewSystemStateRo().SimulatedTurnWorldStateCopy.GetUnitCombatDBRo());
+				var totalOld = oldStats.hp + oldStats.energyShield;
+				var totalNew = newStats.hp + newStats.energyShield;
+				return totalOld - totalNew;
+			}
+
+		}
+	}
 }
