dc5dc81ac91a089a7d49db30462b4d7bb9a2b82f
Bilal <aslambilal@gmail.com>
2023-11-02 12:54:27 -0400

    Looks like its working yay, now to figure out how to create a release

--- Projected Damage/Plugin.cs
+++ Projected Damage/Plugin.cs
@@ -19,7 +19,7 @@ namespace Projected_Damage
 	{
 		public const string PLUGIN_GUID = "Projected_Damage";
 		public const string PLUGIN_NAME = "Projected Damage";
-		public const string PLUGIN_VERSION = "0.0.0";
+		public const string PLUGIN_VERSION = "0.5.0";
 		public static readonly Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
 		internal static ManualLogSource log;
 		private void Awake()
@@ -62,6 +62,7 @@ namespace Projected_Damage
 		{
 			public static GameObject projected_delta_hp;
 			public static GameObject projected_delta_shield;
+			public static GameObject projected_delta_total;
 
 
 			public static void Projected_Delta_HP_Colors(int delta_hp, Transform transform)
@@ -71,11 +72,11 @@ namespace Projected_Damage
 				{
 					transform.GetComponent<TextMeshProUGUI>().color = new Color(1f, 1f, 1f, 0f);
 				}
-				else if ((delta_hp * -1) < 0) // taking damage
+				else if ((delta_hp * -1) < 0) // taking damage same as incoming damage color
 				{
 					transform.GetComponent<TextMeshProUGUI>().color = new Color(1.000f, .478f, .431f, 1f);
 				}
-				else if ((delta_hp * -1) > 0) // healing
+				else if ((delta_hp * -1) > 0) // healing green
 				{
 					transform.GetComponent<TextMeshProUGUI>().color = new Color(.353f, 0.957f, .486f, 1f);
 				}
@@ -102,16 +103,60 @@ namespace Projected_Damage
 
 			}
 
+			public static void Projected_Delta_Total_Colors(int delta_total, Transform transform)
+			{
+
+				if (delta_total == 0) // 0 then set alpha to 0
+				{
+					transform.GetComponent<TextMeshProUGUI>().color = new Color(1f, 1f, 1f, 0f);
+				}
+				else if ((delta_total * -1) < 0) // taking damage same as incoming damage color
+				{
+					transform.GetComponent<TextMeshProUGUI>().color = new Color(1f, 1f, 1f, 1f); // old red color 1.000f, .478f, .431f
+				}
+				else if ((delta_total * -1) > 0) // healing green
+				{
+					transform.GetComponent<TextMeshProUGUI>().color = new Color(.353f, 0.957f, .486f, 1f);
+				}
+				// TODO if dieing add one more color
+
+			}
+
+			private static string Format_Damage_Number(long num)
+			{
+				// Ensure number has max 3 significant digits (no rounding up can happen)
+				long i = (long)Math.Pow(10, (int)Math.Max(0, Math.Log10(num) - 2));
+				num = num / i * i;
+
+				if (num >= 1000000000)
+					return (num / 1000000000D).ToString("0.##") + "B"; // Will this ever be necessary?
+				if (num >= 1000000)
+					return (num / 1000000D).ToString("0.##") + "M";
+				if (num >= 1000)
+					return (num / 1000D).ToString("0.##") + "K";
+
+				if ( num < 0 )
+				{
+					// if the unit will be healing add a + sign to the number
+					return "+" + (num*-1).ToString("#,0");
+				}
+				else
+				{
+					return num.ToString("#,0");
+				}
+
+
+			}
+
+
 			[HarmonyPrefix]
 			public static void HpBarUI_before_Set(HpEnergyShield currentHpEnergyShield, HpEnergyShield predictedHpEnergyShield, HpBarUI __instance)
 			{
 
 				int delta_hp = currentHpEnergyShield.hp - predictedHpEnergyShield.hp;
 				int delta_shield = currentHpEnergyShield.energyShield - currentHpEnergyShield.energyShield;
+				int delta_total = delta_hp + delta_shield;
 
-
-				// log.LogInfo($"delta_hp: {delta_hp}");
-				// log.LogInfo($"delta_shield: {delta_shield}");
 
 				// Check hierarchy and dont display on party display
 				// WorldUI/PartyScreen/CharacterPartyDisplay 1/Player Info/Banner/In Run Stats/HP MP Container/
@@ -121,7 +166,6 @@ namespace Projected_Damage
 
 				// if hierarchy matches the following, display it
 				// WorldUI/OverheadScreen/OverheadUIs/OverheadUI(Clone)/OverheadHpDisplay/Container Medium/
-
 				if (__instance.transform.parent.name == "Container Medium")
 				{
 
@@ -133,7 +177,7 @@ namespace Projected_Damage
 
 					foreach (Transform child in HpBarUIchildren)
 					{
-						if (child.name == "Projected Delta HP")
+/*						if (child.name == "Projected Delta HP")
 						{
 							labelFound = true;
 							// If delta_hp <= 0 then show 
@@ -145,6 +189,12 @@ namespace Projected_Damage
 							labelFound = true;
 							child.GetComponent<TextMeshProUGUI>().text = $"{(delta_shield * -1 <= 0 ? delta_shield * -1 : "+" + (delta_shield * -1))} Shield";
 							Projected_Delta_Shield_Colors(delta_shield, child);
+						}*/
+						if (child.name == "Projected Delta Total")
+						{
+							labelFound = true;
+							child.GetComponent<TextMeshProUGUI>().text = Format_Damage_Number(delta_total);
+							Projected_Delta_Total_Colors(delta_total, child);
 						}
 					}
 
@@ -153,7 +203,7 @@ namespace Projected_Damage
 					{
 						// TODO: Instantiate label from another gameobject to copy the style
 						// myLabelObject = new("Projected Damage Label", typeof(RectTransform), typeof(TextMeshProUGUI));
-
+/*
 						projected_delta_hp = GameObject.Instantiate(__instance.transform.parent.Find("Enemy Name").gameObject);
 						projected_delta_hp.name = "Projected Delta HP";
 						projected_delta_hp.transform.localPosition = new Vector3(0.0f, 65.0f, 0.0f);
@@ -170,7 +220,21 @@ namespace Projected_Damage
 						projected_delta_shield.transform.SetParent(__instance.transform, false);
 						projected_delta_shield.GetComponent<TextMeshProUGUI>().text = $"{delta_shield * -1} Shield";
 
-						Projected_Delta_Shield_Colors(delta_shield, projected_delta_shield.transform);
+						Projected_Delta_Shield_Colors(delta_shield, projected_delta_shield.transform);*/
+
+						projected_delta_total = GameObject.Instantiate(__instance.transform.parent.Find("HP Text").gameObject);
+						projected_delta_total.name = "Projected Delta Total";
+						
+						// right of health bar ( 95 0 0 )
+						// between incoming damage and health ( -15 24 0 )
+						projected_delta_total.transform.localPosition = new Vector3(95.0f, 0.0f, 0.0f);
+						
+						projected_delta_total.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
+						projected_delta_total.transform.SetParent(__instance.transform, false);
+						projected_delta_total.GetComponent<TextMeshProUGUI>().text = Format_Damage_Number( delta_total );
+
+						Projected_Delta_Shield_Colors(delta_total, projected_delta_total.transform);
+
 
 
 
